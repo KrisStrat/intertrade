5b17446df8400d2c314c9a3452bad499347e587b
KrisStrat <62306057+KrisStrat@users.noreply.github.com>
2023-05-18 21:36:58 +0300

    Add files via upload
    Uploaded all files

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+                isMobile.Android()
+                || isMobile.BlackBerry()
+                || isMobile.iOS()
+                || isMobile.Opera()
+                || isMobile.Windows()
+                );
+    }
+};
+
+const iconMenu = document.querySelector('.menu_icon');
+if (iconMenu) {
+    const menuBody =document.querySelector('.nav_menu');
+    iconMenu.addEventListener("click", function(e) {
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('__active');
+    })
+}
